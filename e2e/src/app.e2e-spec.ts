8bd7f6c667ae8dbc466f8e3d1d93805051c5a186
Clemens Ulbrich <clemens.ulbrich@outlook.de>
2019-03-07 18:25:42 +0100

    Fix test configuration

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to clemensulbrich!');
+    expect(page.getTitleText()).toEqual('I´m Clemens Ulbrich');
   });
 
   afterEach(async () => {
